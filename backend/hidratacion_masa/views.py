adf79a5f6bc663752770e3fe4eaffa82fdeb363d
LunaChristian <luna.christian.javier@gmail.com>
2025-11-25 19:12:55 +0100

    Return del calculo de hidratacion en views.py

--- backend/hidratacion_masa/views.py
+++ backend/hidratacion_masa/views.py
@@ -1,4 +1,6 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import HidratacionMasaSerializer
 # Create your views here.
@@ -10,3 +12,20 @@ class CalculoHidratacionMasa(APIView):
     
     def post(self, request):
         serializer = HidratacionMasaSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            cantidad_harina = serializer.validated_data['cantidad_harina']
+            cantidad_agua = serializer.validated_data['cantidad_agua']
+            
+            hidratacion = (cantidad_agua / cantidad_harina) * 100
+            
+            return Response(
+                {
+                    "hidratacion": hidratacion,
+                    "detalle": {
+                        "Harina total": cantidad_harina,
+                        "Agua total": cantidad_agua,
+                    }
+                },
+                status=status.HTTP_200_OK
+            )
